02b54981071f421e921fd4d78ca4e1f1eae57dd9
chuckmuda <charliezmuda@gmail.com>
2017-11-27 11:58:00 -0600

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php
 
-/*database configs ...user-charliez_charlie, pass-a1232123*/
+/*database configs ...username, password, and database empty for public*/
 $db_host = 'localhost';
-$db_user = 'charliez_charlie';
-$db_pass =  'a1232123';
-$db_database = 'charliez_hosting';
+$db_user = '';
+$db_pass =  '';
+$db_database = '';
 
 
 
